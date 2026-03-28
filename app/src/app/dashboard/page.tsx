2b4aa56098ec89747b50d704ae84e5ba7d3e4079
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2026-03-28 01:57:39 +0100

    changed startpage from dashboard to login

---
+++ app/src/app/dashboard/page.tsx
@@ -0,0 +1,26 @@
+import DashboardTile from "@/components/dashboardTile";
+
+export default function Dashboard() {
+    const dashboardTiles = [
+        { id: 1, label: 'Kunder', href: '/kundeoversigt', icon: '📋' },
+        { id: 2, label: 'Opret tilbud', href: '/tilbud', icon: '📝' },
+        { id: 3, label: 'Salg', href: '/salg', icon: '📈' },
+    ];
+
+    return (
+        <div>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                {dashboardTiles.map((tile) => (
+                    <DashboardTile
+                        key={tile.id}
+                        label={tile.label}
+                        href={tile.href}
+                        icon={tile.icon}
+                    />
+                ))}
+            </div>
+
+        </div>
+    );
+}
+
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,29 +1,8 @@
-import Link from 'next/link';
-import DashboardTile from '@/components/dashboardTile';
-const dashboardTiles = [
-  { id: 1, label: 'Kunder', href: '/kundeoversigt', icon: '📋' },
-  { id: 2, label: 'Opret tilbud', href: '/tilbud', icon: '📝' },
-  { id: 3, label: 'Salg', href: '/salg', icon: '📈' },
-];
-
+import { redirect } from 'next/dist/client/components/navigation';
 
 export default function Home() {
-  return (
-    <div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        {dashboardTiles.map((tile) => (
-          <DashboardTile
-            key={tile.id}
-            label={tile.label}
-            href={tile.href}
-            icon={tile.icon}
-          />
-        ))}
-      </div>
 
-    </div>
-  );
+  redirect("/auth/login");
 }
-
 
 
